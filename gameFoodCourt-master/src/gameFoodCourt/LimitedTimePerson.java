3579cf1d6a9436d6bbd8345511b0c974948a9157
Beamryan <beamr@mail.gvsu.edu>
2017-04-10 09:29:39 -0400

    Small Necessary Changes

--- gameFoodCourt-master/src/gameFoodCourt/LimitedTimePerson.java
+++ gameFoodCourt-master/src/gameFoodCourt/LimitedTimePerson.java
@@ -1,36 +1,9 @@
 package gameFoodCourt;
 
-/** Created by Ryan Beam */
 public class LimitedTimePerson extends Person {
 	
-	private int tickTime;
-	private Eatery Destination;
 	
-	// max time person stays in line
-	protected double boothTime;
-	
-	public double getBoothTime() {
-		return boothTime;
+	public LimitedTimePerson(int tickTime, double eateryTime, double leaveTime, double checkoutTime) {
+		super(tickTime, ( (int) eateryTime) * 0.5, ( (int) leaveTime) * 0.5, checkoutTime);
 	}
-	
-	public Eatery getDestination() {
-		return Destination;
-	}
-
-	public void setDestination(Eatery destination) {
-		Destination = destination;
-	}
-	
-	public int getTickTime() {
-		return tickTime;
-	}
-
-	public void setTickTime(int tickTime) {
-		this.tickTime = tickTime / 2;
-	}
-
-	public void setEateryTime(double time) {
-		this.boothTime = time / 2;
-	}
-
 }
